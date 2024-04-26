065e9131aae318c159f30a008034b48203c7ba1d
Lukas <wi23b019@technikum-wien.at>
2024-04-26 22:58:14 +0200

    added 2 test cases for racetrack

--- src/test/java/RaceTrackTest.java
+++ src/test/java/RaceTrackTest.java
@@ -1,4 +1,17 @@
-import static org.junit.jupiter.api.Assertions.*;
-class RaceTrackTest {
-  
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RaceTrackTest {
+    @Test
+    public void constructorTest() {
+        RaceTrack raceTrack = new RaceTrack("Test Track", 5000);
+        assertEquals("Test Track", raceTrack.getName());
+        assertEquals(5000, raceTrack.getLength());
+    }
+
+    @Test
+    public void getNameTest() {
+        RaceTrack raceTrack = new RaceTrack("Test Track", 5000);
+        assertEquals("Test Track", raceTrack.getName());
+    }
 }
