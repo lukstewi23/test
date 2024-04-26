6cb3366ff46ea600121f6780b07cc05f8aec8fe7
Lukas <wi23b019@technikum-wien.at>
2024-04-26 22:05:21 +0200

    changed racetrack

--- src/main/java/RaceTrack.java
+++ src/main/java/RaceTrack.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class RaceTrack {
+public class RaceTrack {
+    private String name;
+    private int length;
+
+    public RaceTrack(String name, int length) {
+        this.name = name;
+        this.length = length;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getLength() {
+        return length;
+    }
 }
