0c7ca1d5c4a4d309825e00ea4df2532c66709b0a
Lukas <wi23b019@technikum-wien.at>
2024-04-26 21:32:29 +0200

    fixed car and manufacturer class

---
+++ src/main/java/Manufacturer.java
@@ -0,0 +1,17 @@
+public class Manufacturer {
+    private String name;
+    private String country;
+
+    public Manufacturer(String name, String country) {
+        this.name = name;
+        this.country = country;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+}
