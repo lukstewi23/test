6dc82bc0141612acc5edcb91a8a6f79c015e68e0
Lukas <wi23b019@technikum-wien.at>
2024-04-26 21:49:15 +0200

    refined the race class

--- src/main/java/Race.java
+++ src/main/java/Race.java
@@ -1,7 +1,55 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Race {
     private String name;
     private int distance;
     private int maxParticipants;
     private int currentParticipants;
+    private List<Car> cars;
+    private List<Manufacturer> manufacturers;
 
+    public Race(String name, int distance, int maxParticipants) {
+        this.name = name;
+        this.distance = distance;
+        this.maxParticipants = maxParticipants;
+        this.currentParticipants = 0;
+        this.cars = new ArrayList<>();
+        this.manufacturers = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public int getMaxParticipants() {
+        return maxParticipants;
+    }
+
+    public int getCurrentParticipants() {
+        return currentParticipants;
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public List<Manufacturer> getManufacturers() {
+        return manufacturers;
+    }
+
+    public void addCar(Car car) {
+        if (currentParticipants < maxParticipants) {
+            cars.add(car);
+            currentParticipants++;
+        }
+    }
+
+    public void addManufacturer(Manufacturer manufacturer) {
+        manufacturers.add(manufacturer);
+    }
 }
